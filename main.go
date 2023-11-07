7fd69fc41051674a62a7376ae496a42a6d5309b9
bujosa <davidbujosa@gmail.com>
2023-11-07 17:49:14 -0400

    feat: add simple example using redis in main

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-redis/redis"
+)
+
+var client *redis.Client
+
 func main() {
-	println("Hello World")
+    client = redis.NewClient(&redis.Options{
+        Addr:     "redis:6379",
+        Password: "", // no password set
+        DB:       0,  // use default DB
+    })
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        pong, err := client.Ping().Result()
+        if err != nil {
+            http.Error(w, "Failed to connect to Redis", http.StatusInternalServerError)
+            return
+        }
+        fmt.Fprintf(w, "Redis Ping Response: %s", pong)
+    })
+
+    http.ListenAndServe(":8080", nil)
 }
